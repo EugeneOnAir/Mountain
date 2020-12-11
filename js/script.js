944718ce4384677940b3e88d32dfb81117a9fbf3
EugeneOnAir <johnnybox229@mail.ru>
2020-12-12 02:59:55 +0300

    full layout

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
-let burger = document.querySelector('.nav-burger');
-let burgerMenu = document.querySelector('.nav');
-let toggleMenu = function(){
-		burger.classList.toggle('active');
-		burgerMenu.classList.toggle('active'); 
-		document.body.classList.toggle('lock');
+let progressLine = document.querySelector('.progress-line');
+window.addEventListener('scroll', progressBar);
+       
+function progressBar(e) {
+   var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+   var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight; 
+   var statusProgressLine = windowScroll / windowHeight * 100;
+   progressLine.style.height = statusProgressLine + '%';
 }
-
-
-burger.addEventListener('click', function(e){
-		e.stopPropagation();
-		toggleMenu();
-	});
-
-document.addEventListener('click', function(e) {
-    let target = e.target;
-    let itsMenu = target == burgerMenu || burgerMenu.contains(target);
-    let itsBurger = target == burger;
-    let isMenuActive = burgerMenu.classList.contains('active');
-    
-    if (!itsMenu && !itsBurger && isMenuActive) {
-        toggleMenu();
-    }
-});
-
-
-const burgerLinks = document.querySelectorAll('.nav-link');
-burgerLinks.forEach(burgerLink => {burgerLink.addEventListener('click', function(){
-		burger.classList.remove('active');
-		burgerMenu.classList.remove('active'); 
-		document.body.classList.remove('lock');
-	});
-})	
-
